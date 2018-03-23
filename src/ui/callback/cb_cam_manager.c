f094f1bafde2d3a864e9cb80104f541ceea9a269
Tac <fmessina@student.42.fr>
2018-03-23 19:57:10 +0100

    cam management is finished!

--- src/ui/callback/cb_cam_manager.c
+++ src/ui/callback/cb_cam_manager.c
@@ -11,7 +11,6 @@ static void	cam_remove(t_env *e, int target)
 	new_array = NULL;
 	if (!(new_array = malloc(sizeof(t_cam) * (NCAM - 1))))
 		s_error("\x1b[1;31mCan't recreate cameras array\x1b[0m", e);
-
 	while (i < (int)NCAM)
 	{
 		if (i == target)
@@ -27,16 +26,50 @@ static void	cam_remove(t_env *e, int target)
 	e->cameras = new_array;
 }
 
+static void	cam_add(t_env *e)
+{
+	t_cam		*new_array;
+	int			i;
+
+	i = 0;
+	new_array = NULL;
+	if (!(new_array = malloc(sizeof(t_cam) * (NCAM + 1))))
+		s_error("\x1b[1;31mCan't recreate cameras array\x1b[0m", e);
+	while (i < (int)NCAM + 1)
+	{
+			ft_memcpy(&new_array[i], &e->cameras[i], sizeof(t_cam));
+			i++;
+	}
+	new_array[i - 1].pos.x = 0;
+	new_array[i - 1].pos.y = 0;
+	new_array[i - 1].pos.z = 0;
+	new_array[i - 1].dir.x = 0;
+	new_array[i - 1].dir.y = 0;
+	new_array[i - 1].dir.z = 1;
+	new_array[i - 1].fov = 30;
+	new_array[i - 1].pitch = 0;
+	new_array[i - 1].yaw = 0;
+	new_array[i - 1].roll = 0;
+	free(e->cameras);
+	e->cameras = new_array;
+}
+
 void	cb_cam_manage_add(GtkButton *btn, gpointer data)
 {
 	t_env *e;
 
 	(void)btn;
 	e = data;
-//	e->scene->n_cams++;
+	cam_add(e);
 	gtk_widget_set_sensitive(e->ui->cam_nav_prev_btn, TRUE);
 	gtk_widget_set_sensitive(e->ui->cam_nav_next_btn, TRUE);
 	gtk_widget_set_sensitive(e->ui->cam_nav_del_btn, TRUE);
+	e->scene->n_cams++;
+	if (!(cl_replace_buffer(e->cl, sizeof(t_cam) * NCAM, 3)))
+		s_error("\x1b[2;31mFailed replacing cam buffer\x1b[0m", e);
+	e->scene->active_cam = e->scene->n_cams - 1;
+	ui_cam_set_id(e);	
+	ui_cam_update(e);
 	ft_putendl("cam added");
 }
 
