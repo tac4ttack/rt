632e5c226a1de469571d19cc1b7ea9151430e6d7
Tac <fmessina@student.42.fr>
2018-03-25 17:23:04 +0200

    tweaks on ui, css shit, infobulles ta race etc

--- src/xml/xml_data_shrink.c
+++ src/xml/xml_data_shrink.c
@@ -9,8 +9,8 @@ void		xml_data_shrink(t_env *e, char **attributes, int *i, t_node *node)
 		if (attributes[(*i)][ft_strlen(attributes[*i]) - 1] != '\"')
 			s_error("\x1b[1;31mError in SHRINK from scene\x1b[0m", e);
 		node->shrink = ft_atoi(attributes[(*i)++] + 8);
-		if (node->shrink < 0)
-			s_error("\x1b[1;31mError SHRINK can't be negative\x1b[0m", e);
+		if (node->shrink <= 0)
+			s_error("\x1b[1;31mError SHRINK must be greater than 0\x1b[0m", e);
 	}
 	else
 		s_error("\x1b[1;31mError reading SHRINK from scene\x1b[0m", e);
