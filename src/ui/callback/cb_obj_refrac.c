e8b89b214b5065f6b1502120525d41352b8dfda7
Tac <fmessina@student.42.fr>
2018-03-27 10:23:48 +0200

    fixed wrong value between 0 and 1

--- src/ui/callback/cb_obj_refrac.c
+++ src/ui/callback/cb_obj_refrac.c
@@ -9,6 +9,9 @@ gboolean		cb_obj_refrac(GtkSpinButton *spin, gpointer data)
 	e = data;
 	obj = e->gen_objects->mem + e->target; 
 	value = gtk_spin_button_get_value(spin);
+	if (value > 0 && value < 1)
+		value = 1;
+	gtk_spin_button_set_value(spin, value);
 	obj->refract = (float)value;
 	return TRUE;
 }
