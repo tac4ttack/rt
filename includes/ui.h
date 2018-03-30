82ba0f88434b6467652c605d4120ee1d59a3381f
Tac <fmessina@student.42.fr>
2018-03-30 18:49:53 +0200

    normed ui.h

--- includes/ui.h
+++ includes/ui.h
@@ -1,39 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ui.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/30 18:49:38 by fmessina          #+#    #+#             */
+/*   Updated: 2018/03/30 18:49:40 by fmessina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef UI_H
 # define UI_H
 
 # include "gtk/gtk.h"
 
-typedef struct		s_keystate
+typedef struct			s_keystate
 {
-	unsigned int	k_up : 1;
-	unsigned int	k_down : 1;
-	unsigned int	k_left : 1;
-	unsigned int	k_right : 1;
-	unsigned int	k_esc : 1;
-	unsigned int	k_space : 1;
-	unsigned int	k_nplus : 1;
-	unsigned int	k_nminus : 1;
-	unsigned int	k_n4 : 1;
-	unsigned int	k_n5 : 1;
-	unsigned int	k_n6 : 1;
-	unsigned int	k_n7 : 1;
-	unsigned int	k_n8 : 1;
-	unsigned int	k_n9 : 1;
-	unsigned int	k_w : 1;
-	unsigned int	k_a : 1;
-	unsigned int	k_s : 1;
-	unsigned int	k_d : 1;
-	unsigned int	k_c : 1;
-	unsigned int	k_i : 1;
-	unsigned int	k_j : 1;
-	unsigned int	k_k : 1;
-	unsigned int	k_l : 1;
-	unsigned int	k_u : 1;
-	unsigned int	k_o : 1;
-	unsigned int	k_z : 1;
-	unsigned int	k_1 : 1;
-	unsigned int		: 5;
-}					t_keystate;
+	unsigned int		k_up : 1;
+	unsigned int		k_down : 1;
+	unsigned int		k_left : 1;
+	unsigned int		k_right : 1;
+	unsigned int		k_esc : 1;
+	unsigned int		k_space : 1;
+	unsigned int		k_nplus : 1;
+	unsigned int		k_nminus : 1;
+	unsigned int		k_n4 : 1;
+	unsigned int		k_n5 : 1;
+	unsigned int		k_n6 : 1;
+	unsigned int		k_n7 : 1;
+	unsigned int		k_n8 : 1;
+	unsigned int		k_n9 : 1;
+	unsigned int		k_w : 1;
+	unsigned int		k_a : 1;
+	unsigned int		k_s : 1;
+	unsigned int		k_d : 1;
+	unsigned int		k_c : 1;
+	unsigned int		k_i : 1;
+	unsigned int		k_j : 1;
+	unsigned int		k_k : 1;
+	unsigned int		k_l : 1;
+	unsigned int		k_u : 1;
+	unsigned int		k_o : 1;
+	unsigned int		k_z : 1;
+	unsigned int		k_1 : 1;
+	unsigned int		dummy : 5;
+}						t_keystate;
 
 typedef	struct			s_ui
 {
@@ -42,229 +54,98 @@ typedef	struct			s_ui
 	unsigned int		light_selector;
 	cl_int4				render_pos;
 	t_keystate			keys;
-
-	// core
 	GtkApplication		*app;
 	GtkBuilder			*builder;
-	
-	// css
 	GtkCssProvider		*css_provider;
-	
 	GtkWidget			*main_window;
-		// GtkWidget			*main_box; //
-			// GtkWidget			*main_panels;
-
-				// GtkWidget			*left_panel_scroll; //
-				// 	GtkWidget			*left_panel_viewport; //
-						GtkWidget			*render;
-						//	cairo_t				*cr; //useless ?
-							cairo_surface_t		*surface; 
-							GdkPixbuf			*pixbuf; 
-
-				// GtkWidget			*right_panel_scroll; //
-				// 	GtkWidget			*right_panel_viewport; //
-						// GtkWidget			*right_panel; //
-
-						// GtkWidget			*object_frame; //
-						// 	GtkWidget			*object_frame_label; //
-						// 	GtkWidget			*object_frame_align; //
-						// 		GtkWidget			*object_frame_box; //
-									GtkWidget			*obj_list_box; 
-										// GtkWidget			*obj_list_id; //
-											GtkWidget			*obj_list_id_label;
-									//	GtkWidget			*obj_list_pos; //
-											// GtkWidget			*obj_list_pos_box; //
-												// GtkWidget			*obj_list_pos_label; //
-												GtkWidget			*obj_list_pos_spin_x;
-												GtkWidget			*obj_list_pos_spin_y;
-												GtkWidget			*obj_list_pos_spin_z;
-										// GtkWidget			*obj_list_dir; //
-											// GtkWidget			*obj_list_dir_box; //
-												// GtkWidget			*obj_list_dir_label; //
-												GtkWidget			*obj_list_dir_spin_x;
-												GtkWidget			*obj_list_dir_spin_y;
-												GtkWidget			*obj_list_dir_spin_z;
-										// GtkWidget			*obj_list_diff; //
-											// GtkWidget			*obj_list_diff_box; //
-												// GtkWidget			*obj_list_diff_label; //
-												GtkWidget			*obj_list_diff_spin_x;
-												GtkWidget			*obj_list_diff_spin_y;
-												GtkWidget			*obj_list_diff_spin_z;
-										// GtkWidget			*obj_list_spec; //
-											// GtkWidget			*obj_list_spec_box; //
-												// GtkWidget			*obj_list_spec_label; //
-												GtkWidget			*obj_list_spec_spin_x;
-												GtkWidget			*obj_list_spec_spin_y;
-												GtkWidget			*obj_list_spec_spin_z;
-										// GtkWidget			*obj_list_ref; //
-											// GtkWidget			*obj_list_ref_box; //
-												// GtkWidget			*obj_list_reflex_label; //
-												GtkWidget			*obj_list_reflex_spin;
-												// GtkWidget			*obj_list_refrac_label; //
-												GtkWidget			*obj_list_refrac_spin;
-										// GtkWidget			*obj_list_opacolor; //
-											// GtkWidget			*obj_list_opacolor_box; //
-												// GtkWidget			*obj_list_opacity_label; //
-												GtkWidget			*obj_list_opacity_spin;
-												// GtkWidget			*obj_list_color_label; //
-												GtkWidget			* obj_list_color_btn;
-												GdkRGBA				obj_color;
-
-										GtkWidget			*obj_list_cone;
-											// GtkWidget			*obj_list_cone_box; //
-												// GtkWidget			*obj_cone_angle_label; //
-												GtkWidget			*obj_cone_angle_spin;
-
-										GtkWidget			*obj_list_cylinder;
-											// GtkWidget			*obj_list_cylinder_box; //
-												// GtkWidget			*obj_cylinder_radius_label; //
-												GtkWidget			*obj_cylinder_radius_spin;
-												// GtkWidget			*obj_cylinder_height_label; //
-												GtkWidget			*obj_cylinder_height_spin;
-
-										GtkWidget			*obj_list_ellipsoid;
-											// GtkWidget			*obj_list_ellipsoid_box; //
-												// GtkWidget			*obj_list_ellipsoid_axis_box; //
-													// GtkWidget			*obj_ellipsoid_axis_label; //
-													GtkWidget			*obj_ellipsoid_axis_spin_x;
-													GtkWidget			*obj_ellipsoid_axis_spin_y;
-													GtkWidget			*obj_ellipsoid_axis_spin_z;
-												// GtkWidget			*obj_list_ellipsoid_radius_box; //
-													GtkWidget			*obj_ellipsoid_radius_label;
-													GtkWidget			*obj_ellipsoid_radius_spin;
-
-										
-										GtkWidget			*obj_list_plane;
-											// GtkWidget			*obj_list_plane_box; //
-												// GtkWidget			*obj_plane_shit_label; //
-
-										GtkWidget			*obj_list_sphere;
-											// GtkWidget			*obj_list_sphere_box; //
-												// GtkWidget			*obj_sphere_radius_label; //
-												GtkWidget			*obj_sphere_radius_spin;
-
-										GtkWidget			*obj_list_torus;
-											// GtkWidget			*obj_list_torus_box; //
-												// GtkWidget			*obj_torus_lilradius_label; //
-												GtkWidget			*obj_torus_lilradius_spin; //
-												// GtkWidget			*obj_torus_bigradius_label; //
-												GtkWidget			*obj_torus_bigradius_spin; //
-
-
-									// GtkWidget			*obj_nav_box; //
-										// GtkWidget			*obj_nav_first; //
-											GtkWidget			*obj_nav_prev_btn;
-											GtkWidget			*obj_nav_next_btn;
-											GtkWidget			*obj_nav_del_btn;
-										// GtkWidget			*obj_nav_second; //
-											// GtkWidget			*obj_nav_add_label; //
-											GtkWidget			*obj_nav_add_type_combo;
-											GtkWidget			*obj_nav_add_btn;
-										// GtkWidget			*obj_nav_third; //
-											// GtkWidget			*obj_nav_jump_label; //
-											GtkWidget			*obj_nav_jump_combo;
-						
-						
-						
-						
-						// GtkWidget			*scene_frame; //
-							// GtkWidget			*scene_frame_label; //
-							// GtkWidget			*scene_frame_align; //
-								// GtkWidget			*scene_frame_box; //
-									// GtkWidget			*scene_resolution_box; //
-										// GtkWidget			*scene_resolution_label; //
-										GtkWidget			*scene_resolution_width_spin;
-										GtkWidget			*scene_resolution_height_spin;
-									// GtkWidget			*scene_ambient_box; //
-										// GtkWidget			*scene_ambient_label; //
-										GtkWidget			*scene_ambient_red_spin;
-										GtkWidget			*scene_ambient_green_spin;
-										GtkWidget			*scene_ambient_blue_spin;
-									// GtkWidget			*scene_depth_ss_box; //
-										// GtkWidget			*scene_depth_label; //
-										GtkWidget			*scene_depth_spin;
-										// GtkWidget			*scene_supersampling_label; //
-										GtkWidget			*scene_supersampling_scale; //
-									// GtkWidget			*scene_postproc_box; //
-										// GtkWidget			*scene_postproc_label; //
-										GtkWidget			*scene_postproc_bw_radio;
-										GtkWidget			*scene_postproc_sepia_radio;
-										GtkWidget			*scene_postproc_none_radio; //
-										GtkWidget			*scene_postproc_invert_check;
-
-						// GtkWidget			*cam_frame; //
-							// GtkWidget			*cam_frame_label; //
-							// GtkWidget			*cam_frame_align; //
-								// GtkWidget			*cam_frame_box; //
-									// GtkWidget			*cam_list_box; //
-										// GtkWidget			*cam_list_id; //
-											GtkWidget			*cam_list_id_label;
-										// GtkWidget			*cam_list_pos; // 
-											// GtkWidget			*cam_list_pos_box; // 
-												// GtkWidget			*cam_list_pos_label; // 
-												GtkWidget			*cam_list_pos_spin_x;
-												GtkWidget			*cam_list_pos_spin_y;
-												GtkWidget			*cam_list_pos_spin_z;
-										// GtkWidget			*cam_list_dir; //
-											// GtkWidget			*cam_list_dir_box; //
-												// GtkWidget			*cam_list_dir_label; //
-												GtkWidget			*cam_list_dir_spin_x;
-												GtkWidget			*cam_list_dir_spin_y;
-												GtkWidget			*cam_list_dir_spin_z;
-										// GtkWidget			*cam_list_fov; //
-											// GtkWidget			*cam_list_fov_box; // 
-												// GtkWidget			*cam_list_fov_label;
-												GtkWidget			*cam_list_fov_spin;
-									// GtkWidget			*cam_nav_box;
-										GtkWidget			*cam_nav_prev_btn;
-										GtkWidget			*cam_nav_next_btn;
-										GtkWidget			*cam_nav_add_btn;
-										GtkWidget			*cam_nav_del_btn;
-
-						// GtkWidget			*light_frame; //		
-							// GtkWidget			*light_frame_label; //
-							// GtkWidget			*light_frame_align; //
-								// GtkWidget			*light_frame_box; // 
-									// GtkWidget			*light_list_box; // 
-										// GtkWidget			*light_list_id; //
-											GtkWidget			*light_list_id_label; //
-										// GtkWidget			*light_list_pos; //
-											// GtkWidget			*light_list_pos_box; //
-												// GtkWidget			*light_list_pos_label; //
-												GtkWidget			*light_list_pos_spin_x; //
-												GtkWidget			*light_list_pos_spin_y; //
-												GtkWidget			*light_list_pos_spin_z; //
-										// GtkWidget			*light_list_params; //
-											// GtkWidget			*light_list_params_box; // 
-												// GtkWidget			*light_list_bright_label; //
-												GtkWidget			*light_list_bright_spin; //
-												// GtkWidget			*light_list_shrink_label; //
-												GtkWidget			*light_list_shrink_spin; //
-												// GtkWidget			*light_list_color_label; //
-												GtkWidget			* light_list_color_btn; //
-												GdkRGBA				light_color; //
-									// GtkWidget			*light_nav_box; //
-										GtkWidget			*light_nav_prev_btn; //
-										GtkWidget			*light_nav_next_btn; //
-										GtkWidget			*light_nav_add_btn; //
-										GtkWidget			*light_nav_del_btn; //
-					
-											
-
-
-
-			// GtkWidget			*status_bar; //
-			// GtkWidget			*tool_bar; //
-				GtkWidget			*tool_play_btn; //
-				GtkWidget			*tool_stop_btn; //
-				GtkWidget			*tool_render_btn; //
-				GtkWidget			*tool_export_btn; //
-				GtkWidget			*tool_about_btn; //
-				// GtkWidget			*tool_fps_placeholder; //
-					GtkWidget			*tool_fps_label; //
-	
-	GtkWidget			*export_window; //	
-	GtkWidget			*about_window; //
+	GtkWidget			*render;
+	cairo_surface_t		*surface;
+	GdkPixbuf			*pixbuf;
+	GtkWidget			*obj_list_box;
+	GtkWidget			*obj_list_id_label;
+	GtkWidget			*obj_list_pos_spin_x;
+	GtkWidget			*obj_list_pos_spin_y;
+	GtkWidget			*obj_list_pos_spin_z;
+	GtkWidget			*obj_list_dir_spin_x;
+	GtkWidget			*obj_list_dir_spin_y;
+	GtkWidget			*obj_list_dir_spin_z;
+	GtkWidget			*obj_list_diff_spin_x;
+	GtkWidget			*obj_list_diff_spin_y;
+	GtkWidget			*obj_list_diff_spin_z;
+	GtkWidget			*obj_list_spec_spin_x;
+	GtkWidget			*obj_list_spec_spin_y;
+	GtkWidget			*obj_list_spec_spin_z;
+	GtkWidget			*obj_list_reflex_spin;
+	GtkWidget			*obj_list_refrac_spin;
+	GtkWidget			*obj_list_opacity_spin;
+	GtkWidget			* obj_list_color_btn;
+	GdkRGBA				obj_color;
+	GtkWidget			*obj_list_cone;
+	GtkWidget			*obj_cone_angle_spin;
+	GtkWidget			*obj_list_cylinder;
+	GtkWidget			*obj_cylinder_radius_spin;
+	GtkWidget			*obj_cylinder_height_spin;
+	GtkWidget			*obj_list_ellipsoid;
+	GtkWidget			*obj_ellipsoid_axis_spin_x;
+	GtkWidget			*obj_ellipsoid_axis_spin_y;
+	GtkWidget			*obj_ellipsoid_axis_spin_z;
+	GtkWidget			*obj_ellipsoid_radius_label;
+	GtkWidget			*obj_ellipsoid_radius_spin;
+	GtkWidget			*obj_list_plane;
+	GtkWidget			*obj_list_sphere;
+	GtkWidget			*obj_sphere_radius_spin;
+	GtkWidget			*obj_list_torus;
+	GtkWidget			*obj_torus_lilradius_spin;
+	GtkWidget			*obj_torus_bigradius_spin;
+	GtkWidget			*obj_nav_prev_btn;
+	GtkWidget			*obj_nav_next_btn;
+	GtkWidget			*obj_nav_del_btn;
+	GtkWidget			*obj_nav_add_type_combo;
+	GtkWidget			*obj_nav_add_btn;
+	GtkWidget			*obj_nav_jump_combo;
+	GtkWidget			*scene_resolution_width_spin;
+	GtkWidget			*scene_resolution_height_spin;
+	GtkWidget			*scene_ambient_red_spin;
+	GtkWidget			*scene_ambient_green_spin;
+	GtkWidget			*scene_ambient_blue_spin;
+	GtkWidget			*scene_depth_spin;
+	GtkWidget			*scene_supersampling_scale;
+	GtkWidget			*scene_postproc_bw_radio;
+	GtkWidget			*scene_postproc_sepia_radio;
+	GtkWidget			*scene_postproc_none_radio;
+	GtkWidget			*scene_postproc_invert_check;
+	GtkWidget			*cam_list_id_label;
+	GtkWidget			*cam_list_pos_spin_x;
+	GtkWidget			*cam_list_pos_spin_y;
+	GtkWidget			*cam_list_pos_spin_z;
+	GtkWidget			*cam_list_dir_spin_x;
+	GtkWidget			*cam_list_dir_spin_y;
+	GtkWidget			*cam_list_dir_spin_z;
+	GtkWidget			*cam_list_fov_spin;
+	GtkWidget			*cam_nav_prev_btn;
+	GtkWidget			*cam_nav_next_btn;
+	GtkWidget			*cam_nav_add_btn;
+	GtkWidget			*cam_nav_del_btn;
+	GtkWidget			*light_list_id_label;
+	GtkWidget			*light_list_pos_spin_x;
+	GtkWidget			*light_list_pos_spin_y;
+	GtkWidget			*light_list_pos_spin_z;
+	GtkWidget			*light_list_bright_spin;
+	GtkWidget			*light_list_shrink_spin;
+	GtkWidget			* light_list_color_btn;
+	GdkRGBA				light_color;
+	GtkWidget			*light_nav_prev_btn;
+	GtkWidget			*light_nav_next_btn;
+	GtkWidget			*light_nav_add_btn;
+	GtkWidget			*light_nav_del_btn;
+	GtkWidget			*tool_play_btn;
+	GtkWidget			*tool_stop_btn;
+	GtkWidget			*tool_render_btn;
+	GtkWidget			*tool_export_btn;
+	GtkWidget			*tool_about_btn;
+	GtkWidget			*tool_fps_label;
+	GtkWidget			*export_window;
+	GtkWidget			*about_window;
 }						t_ui;
 
 void					cb_about_btn(GtkButton *btn, gpointer data);
@@ -354,7 +235,7 @@ gboolean				cb_render_keypress(GtkWidget *widget, GdkEvent *event, \
 gboolean				cb_render_keyrelease(GtkWidget *widget, \
 										GdkEvent *event, gpointer data);
 void					cb_render_update_size(GtkContainer *box, gpointer data);
-gboolean				cb_res_height_update(GtkSpinButton *spin, gpointer data);
+gboolean				cb_res_heigh_update(GtkSpinButton *spin, gpointer data);
 gboolean				cb_res_width_update(GtkSpinButton *spin, gpointer data);
 
 gboolean				cb_sphere_radius(GtkSpinButton *spin, gpointer data);
@@ -367,7 +248,7 @@ gboolean				cb_torus_radius_big(GtkSpinButton *spin, gpointer data);
 
 gboolean				gtk_main_loop(gpointer data);
 int						gtk_quit(GtkApplication *app, gpointer data);
-void					init_gtk(GtkApplication* app, gpointer data);
+void					init_gtk(GtkApplication *app, gpointer data);
 GdkRGBA					int_to_gdkrbga(int color);
 int						gdkrgba_to_int(GdkRGBA color);
 
@@ -428,4 +309,3 @@ int						gdkrgba_to_int(GdkRGBA color);
 # define KEY_1				49
 
 #endif
-
