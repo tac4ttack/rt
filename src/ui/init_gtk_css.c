82c0529e1523c0690216215567cc3a5c20bb75fb
Tac <fmessina@student.42.fr>
2018-04-01 12:15:48 +0200

    tweaked the theme and ui, cone angle minimum is now 1

--- src/ui/init_gtk_css.c
+++ src/ui/init_gtk_css.c
@@ -6,7 +6,7 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/30 20:00:32 by fmessina          #+#    #+#             */
-/*   Updated: 2018/03/30 20:00:33 by fmessina         ###   ########.fr       */
+/*   Updated: 2018/04/01 12:14:28 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	init_gtk_css(t_env *e)
 {
 	e->ui->css_provider = gtk_css_provider_new();
 	gtk_css_provider_load_from_path(e->ui->css_provider, \
-									"theme/bidou/gtk.css", NULL);
+									"theme/rt.css", NULL);
 	gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), \
 	GTK_STYLE_PROVIDER(e->ui->css_provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
 }
