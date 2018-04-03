11d0ce717f34f8b2b11bf8475299ccaa53527a68
Tac <fmessina@student.42.fr>
2018-04-03 17:47:31 +0200

    removed debug putendl

--- src/ui/callback/cb_obj_nav_prev.c
+++ src/ui/callback/cb_obj_nav_prev.c
@@ -6,7 +6,7 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/30 20:17:27 by fmessina          #+#    #+#             */
-/*   Updated: 2018/04/03 17:45:12 by fmessina         ###   ########.fr       */
+/*   Updated: 2018/04/03 17:47:13 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ void			cb_obj_nav_prev(GtkButton *btn, gpointer data)
 	obj_index = gen_get_ptr_index(e->gen_objects, obj);
 	if (obj_index == 0)
 	{
-		ft_putendl("case obj_index == 0");
 		obj = (t_object*)gen_get_index_ptr(e->gen_objects, \
 											e->gen_objects->length - 1);
 		e->target = (void*)obj - e->gen_objects->mem;
