fab32ef5e4fab60e9a2ed0109a441c3a31956dde
Tac <fmessina@student.42.fr>
2018-04-15 18:27:38 +0200

    removed flag print

--- src/xml/data/xml_data_flag.c
+++ src/xml/data/xml_data_flag.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   xml_data_flag.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ntoniolo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/14 18:46:47 by ntoniolo          #+#    #+#             */
-/*   Updated: 2018/04/14 18:46:48 by ntoniolo         ###   ########.fr       */
+/*   Updated: 2018/04/15 18:14:09 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ void		xml_data_flag(t_env *e, char **attributes, int *i, t_node *node)
 		if (attributes[(*i)][ft_strlen(attributes[*i]) - 1] != '\"')
 			s_error("\x1b[1;31mError in FLAG from scene\x1b[0m", e);
 		j = 7;
-		printf("%s\n", attributes[(*i)]);
 		while (attributes[(*i)][j] && attributes[(*i)][j] != '\"')
 		{
 			if (attributes[(*i)][j] - 48)
