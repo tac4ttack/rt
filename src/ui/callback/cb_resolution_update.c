240d99bcf471ec098859e28ff5a86f199790d851
Tac <fmessina@student.42.fr>
2018-04-20 16:19:56 +0200

    fixed resolution change leak

--- src/ui/callback/cb_resolution_update.c
+++ src/ui/callback/cb_resolution_update.c
@@ -6,7 +6,7 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/30 20:17:48 by fmessina          #+#    #+#             */
-/*   Updated: 2018/04/20 10:42:01 by fmessina         ###   ########.fr       */
+/*   Updated: 2018/04/20 11:58:49 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,15 @@ void		ui_update_resolution(t_env *e, int width, int height)
 	WIDTH = width;
 	HEIGHT = height;
 	free(e->pixel_data);
-	if (!(e->pixel_data = malloc(sizeof(int) * WIDTH * HEIGHT)))
+	if (!(e->pixel_data = ft_memalloc(sizeof(int) * WIDTH * HEIGHT)))
 		s_error("\x1b[1;31mCan't initialize new pixel buffer\x1b[0m", e);
-	ft_bzero(e->pixel_data, sizeof(int) * WIDTH * HEIGHT);
 	if (!(e->cuda->update_buffer(e->cuda, WIDTH * HEIGHT * 4, 0)))
 		s_error("\x1b[2;31mFailed replacing Cuda pixel buffer\x1b[0m", e);
 	gtk_widget_set_size_request(e->ui->render, WIDTH, HEIGHT);
+	g_object_unref(e->ui->pixbuf);
 	e->ui->pixbuf = gdk_pixbuf_new_from_data((const guchar *)e->pixel_data, \
 					GDK_COLORSPACE_RGB, 1, 8, WIDTH, \
 					HEIGHT, WIDTH * 4, NULL, NULL);
-	e->ui->surface = NULL;
 	clear_surface(e);
 }
 
